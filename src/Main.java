702215b2de7260517869bb0b3ce6d1a2114ab5a8
ErickAlbertoVR25 <erickalbertovr25@gmail>
2023-09-22 15:29:32 -0600

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * ITESS-TICS
+ * Taller de investigacion
+ * Intento de certificacion java 0Z1-808
+ * By EAVR
+ * 22/Septiembre/2023
+ */
+/**
+ *
+ * @author erick
+ */
+public class Main {
+    public static void main (StrI)
+}
